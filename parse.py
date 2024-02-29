3648f61014b7b80d3c0cd9a9790f2c326d85e011
Martina Hromadkova <xhroma15@vutbr.cz>
2024-02-29 02:55:20 +0100

    Created dictionaries for data types and operands. Added instructions for IPPcode24. Deleted stats for now.

--- parse.py
+++ parse.py
@@ -6,31 +6,195 @@ Datum: 18. 2. 2024
 Popis: Tento skript slouží k analýze kódu v IPPcode24 a generování XML reprezentace.
 """
 
+import re
 import sys
 import argparse
+from xml.etree.ElementTree import Element, SubElement, tostring
 
-# Chybové návratové kódy
-ERROR_INVALID_COMBINATION = 10
-ERROR_DUPLICATE_STATS_FILE = 12
-ERROR_INVALID_HEADER = 21
-ERROR_INVALID_OPCODE = 22
-ERROR_LEXICAL_SYNTAX = 23
+# Konstanty a výčty
 
-# Návratový kód pro úspěšné ukončení
-SUCCESS = 0
+# Výčet datových typů
+DTYPE = {
+    'int': 0,
+    'bool': 1,
+    'string': 2,
+    'nil': 3,
+    'float': 4,
+}
 
-# Slovník pro uchování statistik
-statistics = {
-    '--loc': 0,
-    '--comments': 0,
-    '--labels': 0,
-    '--jumps': 0,
-    '--fwjumps': 0,
-    '--backjumps': 0,
-    '--badjumps': 0,
-    '--frequent': set(),
-    '--print': [],
-    '--eol': 0
+# Výčet operandů
+OPERAND = {
+    'var': 1,
+    'symb': 2,
+    'label': 3,
+    'type': 4,
+}
+
+# Instrukční sada IPPcode24
+INSTR = {
+    # Instrukce programových rámců
+    'MOVE': {
+        'id': 1,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]'],
+    },
+    'CREATEFRAME': {
+        'id': 2,
+        'argt': [],
+    },
+    'PUSHFRAME': {
+        'id': 3,
+        'argt': [],
+    },
+    'POPFRAME': {
+        'id': 4,
+        'argt': [],
+    },
+    'DEFVAR': {
+        'id': 5,
+        'argt': ['OPERAND[var]'],
+    },
+    'CALL': {
+        'id': 6,
+        'argt': ['OPERAND[label]'],
+    },
+    'RETURN': {
+        'id': 7,
+        'argt': [],
+    },
+    # Instrukce datového zásobníku
+    'PUSHS': {
+        'id': 8,
+        'argt': ['OPERAND[symb]'],
+    },
+    'POPS': {
+        'id': 9,
+        'argt': ['OPERAND[var]'],
+    },
+    # Aritmetické a datové instrukce
+    'ADD': {
+        'id': 10,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'SUB': {
+        'id': 11,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'MUL': {
+        'id': 12,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'IDIV': {
+        'id': 13,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'LT': {
+        'id': 14,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'GT': {
+        'id': 15,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'EQ': {
+        'id': 16,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'AND': {
+        'id': 17,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'OR': {
+        'id': 18,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'NOT': {
+        'id': 19,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]'],
+    },
+    'INT2CHAR': {
+        'id': 20,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]'],
+    },
+    'STRI2INT': {
+        'id': 21,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    # Vstupno-výstupní instrukce
+    'READ': {
+        'id': 22,
+        'argt': ['OPERAND[var]', 'OPERAND[type]'],
+    },
+    'WRITE': {
+        'id': 23,
+        'argt': ['OPERAND[symb]'],
+    },
+    # Instrukce pro práci s řetězci
+    'CONCAT': {
+        'id': 24,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'STRLEN': {
+        'id': 25,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]'],
+    },
+    'GETCHAR': {
+        'id': 26,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'SETCHAR': {
+        'id': 27,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    # Instrukce typů
+    'TYPE': {
+        'id': 28,
+        'argt': ['OPERAND[var]', 'OPERAND[symb]'],
+    },
+    # Instrukce na řízení toku programu
+    'LABEL': {
+        'id': 29,
+        'argt': ['OPERAND[label]'],
+    },
+    'JUMP': {
+        'id': 30,
+        'argt': ['OPERAND[label]'],
+    },
+    'JUMPIFEQ': {
+        'id': 31,
+        'argt': ['OPERAND[label]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'JUMPIFNEQ': {
+        'id': 32,
+        'argt': ['OPERAND[label]', 'OPERAND[symb]', 'OPERAND[symb]'],
+    },
+    'EXIT': {
+        'id': 33,
+        'argt': ['OPERAND[symb]'],
+    },
+    # Instrukce na ladění
+    'DPRINT': {
+        'id': 34,
+        'argt': ['OPERAND[symb]'],
+    },
+    'BREAK': {
+        'id': 35,
+        'argt': [],
+    },
+
+}
+
+# Nastavení výstupu chybových hlášek
+
+# Návratový kód
+RETCODE = {
+    'OK': 0,
+    'EPARAM': 10,
+    'ENOENT': 11,
+    'EWRITE': 12,
+    'ENOHEAD': 21,
+    'EOPCODE': 22,
+    'EANLYS': 23,
+    'EINT': 99,
 }
 
 # Funkce pro zpracování vstupního kódu a generování XML reprezentace
@@ -41,50 +205,26 @@ def parse_code(input_code):
 # Funkce pro výpis nápovědy
 def print_help():
     help_message = """
-    Použití: parse.py [--help] [--stats=file] [--loc] [--comments] [--labels] [--jumps] [--fwjumps] [--backjumps] [--badjumps] [--frequent] [--print=string] [--eol]
+    Použití: parse.py [--help || -h]
 
     IPPcode24 Parser
+    Zpracuje kód (IPPcode24) ze standardního vstupu.
+    Pokud nenajde žádné syntaktické nebo lexikální chyby,
+    vypíše jeho XML reprezentaci na standardní výstup.
 
     Volby:
-      --help           Zobrazí tuto nápovědu a ukončí program
-      --stats=file     Zadání souboru pro uložení statistik
-      --loc            Vypíše počet řádků s instrukcemi
-      --comments       Vypíše počet řádků obsahujících komentáře
-      --labels         Vypíše počet definovaných návěští
-      --jumps          Vypíše počet všech instrukcí návratů, skoků a volání
-      --fwjumps        Vypíše počet dopředných skoků
-      --backjumps      Vypíše počet zpětných skoků
-      --badjumps       Vypíše počet skoků na neexistující návěští
-      --frequent       Vypíše nejčastější operační kódy
-      --print=string   Vypíše řetězec string
-      --eol            Vypíše odřádkování
+      --help nebo -h          Zobrazí tuto nápovědu a ukončí program
     """
     print(help_message)
-    sys.exit(SUCCESS)
-      
-# Funkce pro zpracování statistik
-def process_stats(filename):
-    # Zde proběhne zpracování a zápis statistik do souboru
-    pass
+    sys.exit(RETCODE['OK'])
 
 # Hlavní funkce pro analýzu přepínačů a volání odpovídajících funkcí
 def main():
-    # Inicializace argument parseru s popisem programu a bez automatického přidání help přepínače
+    # Inicializace parseru argumentů s popisem programu a bez automatického přidání help přepínače
     parser = argparse.ArgumentParser(description='IPPcode24 Parser', add_help=False)
     
     # Přidání možnosti --help pro zobrazení nápovědy
-    parser.add_argument('--help', action='store_true', help='Zobrazí tuto nápovědu a ukončí program')
-    parser.add_argument('--stats', metavar='filename', help='Zadání souboru pro uložení statistik')
-    parser.add_argument('--loc', action='store_true', help='Vypíše počet řádků s instrukcemi')
-    parser.add_argument('--comments', action='store_true', help='Vypíše počet řádků obsahujících komentáře')
-    parser.add_argument('--labels', action='store_true', help='Vypíše počet definovaných návěští')
-    parser.add_argument('--jumps', action='store_true', help='Vypíše počet všech instrukcí návratů, skoků a volání')
-    parser.add_argument('--fwjumps', action='store_true', help='Vypíše počet dopředných skoků')
-    parser.add_argument('--backjumps', action='store_true', help='Vypíše počet zpětných skoků')
-    parser.add_argument('--badjumps', action='store_true', help='Vypíše počet skoků na neexistující návěští')
-    parser.add_argument('--frequent', action='store_true', help='Vypíše nejčastější operační kódy')
-    parser.add_argument('--print', metavar='string', help='Vypíše řetězec string')
-    parser.add_argument('--eol', action='store_true', help='Vypíše odřádkování')
+    parser.add_argument('--help', '-h', action='store_true', help='Zobrazí tuto nápovědu a ukončí program')
 
     # Zpracování předaných argumentů
     args = parser.parse_args()
@@ -92,24 +232,11 @@ def main():
     # Kontrola, zda byly předány další argumenty kromě přepínače --help
     if len(sys.argv) > 2:
         print("Chyba: Přepínač --help nelze kombinovat s žádným jiným parametrem.", file=sys.stderr)
-        sys.exit(ERROR_INVALID_COMBINATION)    
+        sys.exit(RETCODE['EPARAM'])    
         
     # Pokud byl předán argument --help
     if args.help:
         print_help()
-        
-    # Kontrola chyby 10 pro statistiky
-    if any([args.loc, args.comments, args.labels, args.jumps, args.fwjumps, args.backjumps, args.badjumps, args.frequent, args.print, args.eol]) and not args.stats:
-        print("Chyba: Parametry statistik nelze zadat bez předchozího zadání parametru --stats.", file=sys.stderr)
-        sys.exit(ERROR_INVALID_COMBINATION)
-
-    # Kontrola chyby 12 pro statistiky
-    if args.stats and (args.loc or args.comments or args.labels or args.jumps or args.fwjumps or args.backjumps or args.badjumps or args.frequent or args.print or args.eol):
-        print("Chyba: Nelze zapisovat více skupin statistik do stejného souboru během jednoho spuštění.", file=sys.stderr)
-        sys.exit(ERROR_DUPLICATE_STATS_FILE)
-        
-    if args.stats:
-        process_stats(args.stats)
 
 # Zavolání hlavní funkce
 if __name__ == "__main__":
